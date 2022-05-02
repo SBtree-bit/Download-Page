dea3e21c673eb5d0f8420f020d1c6b12bc200944
SBtree-bit <54784723+SBtree-bit@users.noreply.github.com>
2022-05-02 08:49:27 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+var lib_req = new XMLHttpRequest();
+lib_req.open("GET", "https://cdnjs.cloudflare.com/ajax/libs/jszip/3.9.1/jszip.min.js");
+lib_req.onload = function() {
+  eval(this.responseText);
+  this.open("GET", "https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.0/FileSaver.min.js");
+  this.onload = function() {
+    eval(this.responseText);
+    onCompleteLoad();
+  }
+  this.send();
+}
+lib_req.send();
+function onCompleteLoad() {
+  var html_zip = new JSZip();
+  html_zip.file("index.html", `<!DOCTYPE html><html><head>${document.head.innerHTML}</head><body>${document.body.innerHTML}</body>`);
+  saveAs(html_zip, "download_page.zip");
+}
